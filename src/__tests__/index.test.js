e164c3bb7a0bc36aa224ae827acbc1e228334fa8
Massas <kjtech2020@gmail.com>
2020-11-15 00:03:49 +0900

    expand test case

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -15,4 +15,30 @@ describe('toy-tree', () => {
         expect(stdout.mock.calls[0][0]).toMatchSnapshot();
         expect(code).toBe(0);
     });
+
+    test('-L 2', () => {
+        const code = exec(['fixture', '-L 2']);
+        expect(stdout.mock.calls[0][0]).toMatchSnapshot();
+        expect(code).toBe(0);
+    });
+
+    test('default directory', () => {
+        const code = exec([]);
+        expect(stdout.mock.calls[0][0].split('\n')[0]).toBe('.');
+        expect(code).toBe(0);
+    });
+
+    test('invalid directory', () => {
+        const dir = '__INVALID__DIRECTORY__';
+        const code = exec([dir]);
+        expect(stderr.mock.calls[0][0]).toContain(`Error: "${dir}" does not exist`);
+        expect(code).toBe(1);
+    });
+
+    test('invalid level option', () => {
+        const code = exec(['-L 0']);
+        expect(stderr.mock.calls[0][0]).toContain('Error: Invalid level');
+        expect(code).toBe(1);
+    });
+
 });
